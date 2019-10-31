8cd2014a7816c87daca2346434151f445d714be5
雷博闻 <l17623029272@163.com>
2019-10-31 15:50:22 +0800

    everyone has their dream.

--- join().py
+++ join().py
@@ -1,10 +1,4 @@
-#语法：str.join(sequence)#
-#参数：sequence：要连接的元素序列
-#     str:指定的字符
-#返回值:返回通过指定字符连接序列中元素生成的新字符
-#示例：
-a = ("I","love","you","999")
-b = ""
-c = b.join(a)
-print(c)
-#输出结果为：Iloveyou999
+#突然发现Python输入也是可以使用逗号分隔然后连着输的
+a,b,c=input("1"),input("2"),input("3")
+print(a,b,c)
+#输出结果为：1 2 3
