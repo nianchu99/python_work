cc44a41ea02de8cc151ec88feee7d5760eb84a45
“nianchu”99 <l17623029272@163.com>
2020-08-17 20:30:33 +0800

    everyone has their dream.

--- TestForReviewPython.py
+++ TestForReviewPython.py
@@ -153,7 +153,6 @@ print(Chain().user('michael').repos)
 # success
 """
 
-
 """
 编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径。
 """
@@ -202,17 +201,17 @@ print(email_2.match('<Tom Paris> tom@voyager.org').group(2))
 # answer
 import re
 from datetime import timezone, datetime, timedelta
+
+
 def to_timestamp(dt_str, tz_str):
     dt_com = re.compile(r'UTC([+|-][\d]{1,2}):00')
     tz = dt_com.match(tz_str).group(1)
     int_tz = int(tz)
     set_timezone = timezone(timedelta(hours=int_tz))
     str2dt = datetime.strptime(dt_str, '%Y-%m-%d %H:%M:%S')
-    str2dt_new = str2dt.replace(tzinfo=set_timezone) # 使用replace之后必须复制给另外一个变量，否则tzinfo无法修改
+    str2dt_new = str2dt.replace(tzinfo=set_timezone)  # 使用replace之后必须复制给另外一个变量，否则tzinfo无法修改
 
     return str2dt_new.timestamp()
-
-
 
 
 # 测试:
@@ -221,3 +220,332 @@ assert t1 == 1433121030.0, t1
 t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
 assert t2 == 1433121030.0, t2
 print('Pass')
+
+# collections
+# namedtuple
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+p = Point(1, 2)
+
+print(p.x)
+print(p.y)
+
+"""
+namedtuple是一个函数，用来创建一个自定义的tuple对象，并且规定了tuple元素的个数，并可以用属性而不是索引来引用tuple的某个元素
+下面验证Point的类型：
+"""
+print(isinstance(p, Point))
+print(isinstance(Point, tuple))
+print(isinstance(p, tuple))
+
+# 下面使用namedtuple定义一个圆
+Circle = namedtuple('Circle', ['x', 'y', 'radius'])
+# 创建一个圆形
+circle = Circle(0, 0, 5)
+print("The center of the circle is (%d, %d), and its radius is %s " % (circle.x, circle.y, circle.radius))
+
+# deque
+"""
+deque除了实现list的append()和pop()外，还支持appendleft()和popleft(),这样就可以非常高效地往头部添加或删除元素
+deque是为了高效实现插入和删除操作的双向列表，适合用于队列和栈
+"""
+from collections import deque
+
+q = deque(['a', 'b', 'c', 5])
+print(q)
+q.append('d')
+print(q)
+q.appendleft("The left one")
+print(q)
+q.popleft()
+print(q)
+
+# defaultdict
+'''
+使用dict时，如果引用的Key不存在，就会抛出KeyError。如果希望key不存在时，返回一个默认值，就可以用defaultdict
+默认值是调用函数返回的，函数在创建defaultdict对象时传入
+除了在Key不存在时返回默认值，defaultdict的其他行为跟dict是完全一样的
+'''
+from collections import defaultdict
+
+dd = defaultdict(lambda: 'The default value')
+dd['key1'] = ' The value of key1'
+print(dd['key1'])  # The value of key1
+print(dd['key2'])  # The default value
+
+# OrderedDict
+'''
+dict是无序的，如果要保持Key的顺序，可以使用OrderedDict;
+OrderedDict的Key会按照插入的顺序排列，不是Key本身排序；
+'''
+from collections import OrderedDict
+d = dict(a=1, c = 3, b = 2)  # {'a': 1, 'b': 2, 'c': 3} ——--- Python3.6以后dict也是有序的了。
+print(d)
+# 实现Orderedict
+d1 = OrderedDict(d)
+print(d1)
+# OrderedDict可以实现一个FIFO(先进先出)的dict，当容量超出限制时，先删除最早添加的Key：
+# 下面这个实现没有怎么看懂
+class LastUpdatedOrderedDict(OrderedDict):
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self.__capacity = capacity
+
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self.__capacity:
+            last = self.popitem(last=False)
+            print('remove', last)
+
+        if containsKey:
+            del self[key]
+            print('set', (key, value))
+
+        else:
+            print('add', (key, value))
+        OrderedDict.__setitem__(self, key, value)
+
+# Counter： 一个简单的计数器
+# 使用Counter统一字符串中字符出现的次数
+# Counter就是一个dict
+from collections import Counter
+c = Counter()
+s = 'today is a good day, it\'s a good time to learn something'
+for ch in s:
+    c[ch] = c[ch] + 1
+
+print(c)
+
+# base64
+# 解编码
+import base64
+# 编码
+bs1 = base64.b64encode(b'binary string')
+print(bs1)
+# 解码
+bs2 = base64.b64decode(bs1)
+print(bs2)
+
+# 使用url-safe的base64编码
+'''
+由于标准的Base64编码后可能出现字符+和/，在URL中就不能直接作为参数，所以又有一种"url safe"的base64编码，其实就是把字符+和/分别变成-和_
+'''
+bs3 = base64.b64encode(b'i\xb7\x1d\xfb\xef\xff')
+print(bs3)  # b'abcd++//' ---- 字符+和/没有改变
+bs4 = base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
+print(bs4)  # 字符+和/分别变成了-和_
+
+
+# 小节练习
+def safe_base64_decode(s):
+    if len(s) != 4:
+        s = s + bytes('=', encoding='utf-8') * (4 - len(s) % 4)
+
+    if not isinstance(s, bytes):
+        s = bytes(s, encoding='utf-8')
+
+    # 解码
+    return base64.b64decode(s)
+
+# 测试:
+assert b'abcd' == safe_base64_decode(b'YWJjZA=='), safe_base64_decode('YWJjZA==')
+assert b'abcd' == safe_base64_decode(b'YWJjZA'), safe_base64_decode('YWJjZA')
+print('Pass')
+
+# struct
+# struct的pack函数把任意数据类型变成bytes
+import struct
+s = struct.pack('>I', 10240099)
+print(s)  # b'\x00\x9c@c'
+# 上面>表示： 字节顺序是big-endian，也就是网络序；I表示4字节无符号整数
+# 后面的参数个数要和处理指令一致
+# unpack把bytes变成相应的数据类型
+four_bytes_int, two_bytes_int = struct.unpack('>IH', b'\xf0\xf0\xf0\xf0\x80\x80')
+print(four_bytes_int)  # 4042322160
+print(two_bytes_int)  # 32896
+# H表示2字节无符号整数
+
+# 小节练习
+# 请编写一个bmpinfo.py，可以检查任意文件是否是位图文件，如果是，打印出图片大小和颜色数。
+import struct
+# 请编写一个bmpinfo.py，可以检查任意文件是否是位图文件，如果是，打印出图片大小和颜色数。
+s = b'\x42\x4d\x38\x8c\x0a\x00\x00\x00\x00\x00\x36\x00\x00\x00\x28\x00\x00\x00\x80\x02\x00\x00\x68\x01\x00\x00\x01\x00\x18\x00'
+
+def bmpinfo(s):
+    if not isinstance(s, bytes):
+        s = bytes(s, encoding='utf-8')
+    if s[0:2] == (b'BM' or b'BA'):
+        result = struct.unpack('<ccIIIIIIHH', s)
+        photo_size = result[-4:-2]
+        print("此位图的大小是：%d x %d" % (photo_size[0], photo_size[1]))
+        print("此位图的颜色数是：%d" % result[len(result) - 1])
+
+    else:
+        print("抱歉， 这不是一个位图。")
+
+bmpinfo(s)
+s1 = 'fdsfsdfsdfdsf'
+bmpinfo(s1)
+
+"""
+output:
+b'BM'
+此位图的大小是：640 x 360
+此位图的颜色数是：24
+b'fd'
+抱歉， 这不是一个位图。
+"""
+# hashlib
+import hashlib
+md5 = hashlib.md5()
+md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
+print(md5.hexdigest())
+'''
+MD5是常见的摘要算法，速度很快，生成结果是固定的128bit字节，通常用一个32位的16进制字符串表示。
+'''
+
+"""
+另一种常见的摘要算法是SHA1，调用SHA1和调用MD5完全类似：
+"""
+sh1 = hashlib.sha1()
+sh1.update('how to use sha1 in '.encode('utf-8'))
+sh1.update('python hashlib?'.encode('utf-8'))
+print(sh1.hexdigest())
+""""
+SHA1的结果是160bit字节，通常用一个40位的16进制字符串表示
+"""
+
+import hashlib
+
+
+# 1 - 根据用户输入的口令，计算出存储在数据库中的MD5口令：
+def calc_md5(password):
+    s = str(password)
+    one_md5 = hashlib.md5()
+    one_md5.update(s.encode('utf-8'))
+
+    return one_md5.hexdigest()
+
+
+print(calc_md5("todayisaniceday."))
+
+# 2 - 设计一个验证用户登录的函数，根据用户输入的口令是否正确，返回True或False：
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(user, password):
+    s = str(password)
+    one_md5 = hashlib.md5()
+    one_md5.update(s.encode('utf-8'))
+    return one_md5.hexdigest() == db[str(user)]
+
+
+print(login('michael', 123456))
+print(login('bob', '888888'))
+print(login('alice', password=123456))
+
+
+
+import hashlib
+
+# 根据用户输入的登录名和口令模拟用户注册，计算更安全的MD5：
+salt_value = 'the-Salt'
+db = {}
+def register(username, password):
+    db[username] = get_md5(password + username + salt_value)
+
+
+
+def get_md5(password):
+    s = str(password)
+    one_md5 = hashlib.md5()
+    one_md5.update(s.encode('utf-8'))
+    return one_md5.hexdigest()
+
+
+
+# 测试
+register('bob', 'hello world')
+print(db)  # {'bob': '7fa0c1935fed7e7730be403321f3b04c'}
+register('bowenkei', 'today is a good day')
+print(db)
+
+# 根据修改后的MD5算法实现用户登录的验证：
+def login(user, password):
+    return db[str(user)] == get_md5(str(password) + str(user) + salt_value)
+
+
+# Test
+print(login('bob', 'hello world'))
+print(login('bowenkei', 'today is a good day'))
+print(login('bowenkei', 123456))
+
+# itertools
+# count()会创建一个无限迭代器。
+import itertools
+naturals = itertools.count(100) # count()括号里面的数字指定了从哪一个数字开始数
+print(naturals.__next__())
+print(naturals.__next__())
+
+# cycle() —— 会把传入的一个序列无限重复下去
+abc = itertools.cycle(['a', 'b', 'c'])
+for i in range(3 * 2):  # 这里指定了次数，避免出现死循环
+    print(abc.__next__())
+
+# repeat()负责将一个元素无限重复下去，不过如果提供第二个参数就可以限定重复次数：
+ns = itertools.repeat('A', 3)
+for n in ns:
+    print(n)  # 迭代了三次
+
+# 无限序列虽然可以无限迭代下去，但是通常会通过takenwhile()等函数根据条件判断来截取一个有限的序列：
+naturals = itertools.count(1)
+ns = itertools.takewhile(lambda x:x <= 10, naturals)
+print(ns)
+print(list(ns))
+
+# chain()
+# chain()可以把一组迭代对象串联起来，形成一个更大的迭代器
+new_iter = itertools.chain('ABC', 'XYZ')
+print(list(new_iter))  # ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+# proupby()
+# groupby把迭代器中相邻的重复元素挑出来放在一起：
+for key, group in itertools.groupby('AAABBBBBBDDDDCCCCAAA'):
+    print(key, list(group))
+
+# 向groupby中传入一个函数可以实现忽略大小写：
+for key, group in itertools.groupby('AAABBBBbcdaabccBBDDDDCCCCAAA', lambda c: c.upper()):
+    print(key, list(group))
+
+from xml.parsers.expat import ParserCreate
+class DefaultSaxHandler(object):
+    def start_element(self, name, attrs):
+        print("sax:start_element: %s, attrs: %s" % (name, str(attrs)))
+
+    def end_element(self, name):
+        print("sax:end_element: %s" % name)
+
+    def char_data(self, text):
+        print("sax:char_data: %s " % text)
+
+
+
+xml = r'''<?xml version="1.0"?>
+<ol>
+    <li><a href="/python">Python</a></li>
+    <li><a href="/ruby">Ruby</a></li>
+</ol>
+'''
+handler = DefaultSaxHandler()
+parser = ParserCreate()
+parser.StartElementHandler = handler.start_element
+parser.EndElementHandler = handler.end_element
+parser.CharacterDataHandler = handler.char_data
+parser.Parse(xml)
+
+
+
--- spiderForOuLu.py
+++ spiderForOuLu.py
@@ -1,66 +1,141 @@
-from lxml import etree
+import re
 import requests
+from lxml import etree
+import threading
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-r = requests.get('https://dict.eudic.net/dicts/en/frustration', headers=headers)
+# 将单词解释写入外部文件
+def write_answer_to_file(explanation='', words=''):
+    with open('/Users/bowenkei/Desktop/WordSix.txt', 'a+') as file_object:
+        if words == '' and explanation == '':
+            file_object.write("💗💗💗💗💗💗💗💗💗💗💗💗💗💗\n")
+        elif words == '':
+            file_object.write(str(explanation) + '\n')
+        elif explanation == '':
+            file_object.write(str(words) + '\n')
 
-x = etree.HTML(r.text)
-all_exp = x.xpath('//*[@id="ExpFCChild"]/div')
-print(len(all_exp))
-if len(all_exp) == 1:
-    print("there1")
-    if len(x.xpath('//*[@id="ExpFCChild"]/i')) == 1:
-        print("there2")
-        print(x.xpath('//*[@id="ExpFCChild"]/i')[0].text,
-              x.xpath('//*[@id="ExpFCChild"]/text()')[0])  # part_of_speech, answer
-    elif len(x.xpath('//*[@id="ExpFCChild"]/ol')) == 1:
-        print("have one ol")
-        li_collect = x.xpath('//*[@id="ExpFCChild"]/ol/li')
-        part_of_speech = x.xpath('//*[@id="ExpFCChild"]/ol/li[1]/i')
-        part_answer = x.xpath('//*[@id="ExpFCChild"]/ol/li[1]/text()')
-        print(part_of_speech[0].text, part_answer[0])
-        for i in range(1, len(li_collect)):
-            print(li_collect[i].text)
-
-elif len(all_exp) == 2:
-    print("there3")
-    if len(x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')) != 0:
-        print("ol在div下面")
-        all_exp = x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')
-        print(len(all_exp))
-        for i in range(len(all_exp)):
-            all_exp[i] = all_exp[i].text
-        print(all_exp)
-    elif len(x.xpath('//*[@id="ExpFCChild"]/ol')) != 0:
-        print(len(x.xpath('//*[@id="ExpFCChild"]/ol/li')))
-        for i in range(1, len(x.xpath('//*[@id="ExpFCChild"]/ol/li')) + 1):
-            strs = '//*[@id="ExpFCChild"]/ol/li' + '[' + str(i) + ']'
-            print("---------------")
-            if len(x.xpath(strs + '/i')) != 0:# x.xpath(strs + '/i') != 0:
-                print(x.xpath(strs + '/i/text()') )
-                print(x.xpath(strs + '/text()'))
-            else:
-                print(x.xpath(strs + '/text()'))
-    elif len(x.xpath('//*[@id="ExpFCChild"]/i')) != 0:
-       print(x.xpath('//*[@id="ExpFCChild"]/i/text()') + x.xpath('//*[@id="ExpFCChild"]/text()'))
-    else:
-        print(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()'))
-
-# //*[@id="ExpFCChild"]/ol/li[2]
+        else:
+            file_object.write(str(words) + '-->' + str(explanation) + '\n')
 
 
-elif len(all_exp) == 3:
-    if len(x.xpath('//*[@id="ExpFCChild"]/div[2]/ol')) == 0:
-        print(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()'))
-    else:
-        print("has three divs")
-        all_exp = x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')
-        for i in range(1, len(all_exp) + 1):
-            strs = '//*[@id="ExpFCChild"]/div[2]/ol/li' + '[' + str(i) + ']' + '/text()'
-            print(x.xpath(strs))
+# 爬虫 + 解析
+def search_word(s, word):
+    pass
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    #  'https://dict.eudic.net/dicts/en/attr'
+    r = requests.get(s, headers=headers)
+    x = etree.HTML(r.text)
+
+    all_exp = x.xpath('//*[@id="ExpFCChild"]/div')
+    print(len(all_exp))
+    if len(all_exp) == 1:
+
+        if len(x.xpath('//*[@id="ExpFCChild"]/i')) == 1:
+            write_answer_to_file()
+            write_answer_to_file(word)
+
+            write_answer_to_file(
+                str(x.xpath('//*[@id="ExpFCChild"]/i')[0].text) + str(x.xpath('//*[@id="ExpFCChild"]/text()')[0]))
+            print(x.xpath('//*[@id="ExpFCChild"]/i')[0].text,
+                  x.xpath('//*[@id="ExpFCChild"]/text()')[0])  # part_of_speech, answer
+        elif len(x.xpath('//*[@id="ExpFCChild"]/ol')) == 1:
+            if len(x.xpath('//*[@id="ExpFCChild"]/ol/*[i]')) >= 2:
+                write_answer_to_file()
+                write_answer_to_file(word)
+                len_of_i = len(x.xpath('//*[@id="ExpFCChild"]/ol/*[i]'))
+                all_i = x.xpath('//*[@id="ExpFCChild"]/ol/li/i')
+                all_li = x.xpath('//*[@id="ExpFCChild"]/ol/li')
+                s_of_li = '//*[@id="ExpFCChild"]/ol/li'
+                for i in range(len_of_i):
+                    write_answer_to_file(
+                        str(all_i[i].text) + str(x.xpath(s_of_li + '[' + str(i + 1) + ']' + '/text()')))
+                    print(all_i[i].text, end='')
+                    print(x.xpath(s_of_li + '[' + str(i + 1) + ']' + '/text()'))
+            elif len(x.xpath('//*[@id="ExpFCChild"]/ol/*[i]')) == 1:
+                print("have one ol")
+                li_collect = x.xpath('//*[@id="ExpFCChild"]/ol/li')
+                part_of_speech = x.xpath('//*[@id="ExpFCChild"]/ol/li[1]/i')
+                part_answer = x.xpath('//*[@id="ExpFCChild"]/ol/li[1]/text()')
+                write_answer_to_file()
+                write_answer_to_file(word)
+                write_answer_to_file((part_of_speech[0].text + part_answer[0]))
+                print(part_of_speech[0].text, part_answer[0])
+                for i in range(1, len(li_collect)):
+                    write_answer_to_file(li_collect[i].text)
+                    print(li_collect[i].text)
+
+
+    elif len(all_exp) == 2:
+
+        if len(x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')) != 0:
+
+            all_exp = x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')
+            print(len(all_exp))
+            for i in range(len(all_exp)):
+                all_exp[i] = all_exp[i].text
+            write_answer_to_file()
+            write_answer_to_file(word)
+            write_answer_to_file(all_exp)
+            print(all_exp)
+        elif len(x.xpath('//*[@id="ExpFCChild"]/ol')) != 0:
+            print(len(x.xpath('//*[@id="ExpFCChild"]/ol/li')))
+            write_answer_to_file()
+            write_answer_to_file(word)
+            for i in range(1, len(x.xpath('//*[@id="ExpFCChild"]/ol/li')) + 1):
+                strs = '//*[@id="ExpFCChild"]/ol/li' + '[' + str(i) + ']'
+                print("---------------")
+                if len(x.xpath(strs + '/i')) != 0:  # x.xpath(strs + '/i') != 0:
+                    write_answer_to_file(str(x.xpath(strs + '/i/text()')) + str(x.xpath(strs + '/text()')))
+                    print(x.xpath(strs + '/i/text()'))
+                    print(x.xpath(strs + '/text()'))
+                else:
+                    write_answer_to_file(x.xpath(strs + '/text()'))
+                    print(x.xpath(strs + '/text()'))
+        elif len(x.xpath('//*[@id="ExpFCChild"]/i')) != 0:
+
+            write_answer_to_file()
+            write_answer_to_file(word)
+            write_answer_to_file(
+                str(x.xpath('//*[@id="ExpFCChild"]/i/text()')) + str(x.xpath('//*[@id="ExpFCChild"]/text()')))
+            print(x.xpath('//*[@id="ExpFCChild"]/i/text()') + x.xpath('//*[@id="ExpFCChild"]/text()'))
+        else:
+            write_answer_to_file()
+            write_answer_to_file(word)
+            write_answer_to_file(str(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()')))
+            print(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()'))
+
+    # //*[@id="ExpFCChild"]/ol/li[2]
+
+    elif len(all_exp) == 3:
+        if len(x.xpath('//*[@id="ExpFCChild"]/div[2]/ol')) == 0:
+            write_answer_to_file()
+            write_answer_to_file(word)
+            write_answer_to_file(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()'))
+            print(x.xpath('//*[@id="ExpFCChild"]/div[2]/text()'))
+        else:
+            # print("has three divs")
+            write_answer_to_file()
+            all_exp = x.xpath('//*[@id="ExpFCChild"]/div[2]/ol/li')
+            write_answer_to_file(word)
+            for i in range(1, len(all_exp) + 1):
+                strs = '//*[@id="ExpFCChild"]/div[2]/ol/li' + '[' + str(i) + ']' + '/text()'
+
+                write_answer_to_file(x.xpath(strs))
+                print(x.xpath(strs))
 
 
 
+# Enter all words that you want to look up
+# words = 'slave'
+# list_of_words = re.split(r'[\s\,\;]+', words)
+def start(list_of_words):
+    print(list_of_words)
+    print(len(list_of_words))
+    for i in range(len(list_of_words)):
+        s = 'https://dict.eudic.net/dicts/en/' + list_of_words[i]
+        search_word(s, list_of_words[i])
+
+if __name__ == '__main__':
+    pass
 """
 开发总结： 
 2020.8.14
@@ -73,4 +148,16 @@ elif len(all_exp) == 3:
 2020.8.15
 基本功能已经实现，下一步计划是：
 实现输入一系列单词，程序则获取单词相应的解释后将单词和解释一起写入到外部文件，省去了查六级词汇是每次搜索复制粘贴的麻烦
+
+2020.8.16 
+已经实现前述功能。
+下一步：
+设计一个GUI界面
+2020.8.17 
+GUI实现中，目前问题： 
+GUI调用过程中，是不会执行爬取任务的，只有等GUI关闭之后才能继续爬取。
+似乎解决途径只有一个，那就是使用多线程或者多进程
+
+后来发现tkinter无法使用多线程，最终解决方案是在实现GUI的py文件里面调用此py文件。
+GUI文件是OuLuGUI.py
 """
